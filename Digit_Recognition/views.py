d1f44b5560ac17aa08de7ad46f6af2e93c561835
shawshank-dulal <sashank.dulal@gmail.com>
2023-05-04 12:47:47 +0545

    fixed predictions

--- Digit_Recognition/views.py
+++ Digit_Recognition/views.py
@@ -1,4 +1,7 @@
 from joblib import load
+from PIL import Image
+import pandas as pd
+import numpy as np
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 import os
@@ -7,8 +10,15 @@ import os
 def load_model(model):
     return load(model)
 
+def process_data(file_url):
+    image=Image.open(file_url)
+    scaler=load('scaler.save')
+    
+    image_df=pd.DataFrame(np.array(image).reshape(1,784),columns=[f'pixel{str(i)}' for i in range(784)])
+    scaler.fit_transform(image_df)
+    return image_df
 
-model_save_dir = './exported_models'
+model_save_dir = 'exported_models'
 
 
 def getHome(request):
@@ -19,23 +29,26 @@ def getBestPrediction(request):
     if request.method == "POST":
         algorithm = request.POST.get('algorithm')
         images = request.FILES.getlist('image')
+        print(algorithm)
         predictions = []
         for image in images:
             fs = FileSystemStorage(location='media/images')
             filename = fs.save(image.name, image)
             file_url = fs.url(filename)
-
-            if algorithm == 'All':
+            data=process_data(f'media/images{file_url}')
+            if algorithm == 'all':
                 for saved_model in os.listdir(model_save_dir):
-                    model = load_model(saved_model)
+                    model = load_model(os.path.join(model_save_dir,saved_model))
                     res = model.predict(data)
+                    print(res)
                     model_name = saved_model.split('.')[0]
-                    predictions.append({'model': model_name, 'prediction': res})
+                    predictions.append({'image_dir':f'media/images{file_url}','model': model_name, 'prediction': res})
             else:
-                model = load_model(os.path.join(model_save_dir, 'knn_final.joblib'))
+                model = load_model(os.path.join(model_save_dir,'KNN-CV.joblib'))
                 res = model.predict(data)
                 model_name = ("KNN-CV")
-                predictions.append({'model': model_name, 'prediction': res})
+                
+                predictions.append({'image_dir':f'media/images{file_url}','model': model_name, 'prediction': res})
 
         return render(request, 'main.html', {'predictions': predictions})
     else:
