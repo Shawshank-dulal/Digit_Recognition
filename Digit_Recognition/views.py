bb505bceb44f8c06c9eb4423ad29b3cfd99aa3a3
Takshak Bist <076bct093.takshak@pcampus.edu.np>
2023-05-04 10:45:03 +0545

    [addition]: added dropdown in main.html, small change in getBestPrediction view to handle post request

--- Digit_Recognition/views.py
+++ Digit_Recognition/views.py
@@ -4,12 +4,20 @@ from django.shortcuts import render
 def load_model(model):
     load(model)
 
-
 def getHome(request):
     return render(request,'main.html')
 
 def getBestPrediction(request):
-    return render(request,'main.html')
+    # return render(request,'main.html')
+    if request.method == "POST":
+        algorithm = request.POST.get('algorithm')
+        image = request.FILES.get('image')
+        print(algorithm)
+        prediction = load_model(algorithm).predict(image) # make necessary change in this view
+        return render(request,'main.html',{'prediction':prediction})
+    else:
+        return render(request,'main.html')
+
 
 def getAllPredictions(request):
     pass
