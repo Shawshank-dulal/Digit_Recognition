41eb259a943a8a6c883dc461540c30cec506a440
Takshak Bist <076bct093.takshak@pcampus.edu.np>
2023-05-04 11:52:42 +0545

    [addition]:made changes in getBestPrediction view

--- Digit_Recognition/views.py
+++ Digit_Recognition/views.py
@@ -1,22 +1,41 @@
 from joblib import load
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+import os
 
 def load_model(model):
-    load(model)
+    return load(model)
 
+model_save_dir = './exported_models'
 def getHome(request):
     return render(request,'main.html')
 
 def getBestPrediction(request):
-    # return render(request,'main.html')
     if request.method == "POST":
         algorithm = request.POST.get('algorithm')
-        image = request.FILES.get('image')
-        print(algorithm)
-        prediction = load_model(algorithm).predict(image) # make necessary change in this view
-        return render(request,'main.html',{'prediction':prediction})
+        images = request.FILES.getlist('image')
+        predictions = []
+        for image in images:
+            fs = FileSystemStorage(location='media/images')
+            filename = fs.save(image.name, image)
+            file_url = fs.url(filename)
+
+            if algorithm == 'All':
+                for saved_model in os.listdir(model_save_dir):
+                    model = load_model(saved_model)
+                    res = model.predict(data)
+                    model_name = saved_model.split('.')[0]
+                    predictions.append({'model': model_name, 'prediction': res})
+            else:
+                model = load_model(os.path.join(model_save_dir, 'knn_final.joblib'))
+                res = model.predict(data)
+                model_name = ("KNN-CV")
+                predictions.append({'model': model_name, 'prediction': res})
+
+        return render(request, 'main.html', {'predictions': predictions})
     else:
-        return render(request,'main.html')
+        return render(request, 'main.html')
+
 
 
 def getAllPredictions(request):
